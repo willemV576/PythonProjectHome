37ce5933d3a7fa8aa8c9b4fed2e065f4af957041
willemV576 <willemvel@hotmail.com>
2025-02-06 11:37:57 +0800

    Your commit message describing the change

--- Source/Hello.py
+++ Source/Hello.py
@@ -11,6 +11,9 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('PyCharm2')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+d
