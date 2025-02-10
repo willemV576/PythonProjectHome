12e44d4e7e66ab667bda4669a3e2424149e12cb0
willemV576 <willemvel@hotmail.com>
2025-02-10 19:16:21 +0800

    new test file

--- Source/mock_array.py
+++ Source/mock_array.py
@@ -15,7 +15,6 @@ class Array:
         self.index += 1
         self.array[self.index] = item
 
-
     def get(self, pos):
         if pos > self.index:
             raise IndexError("Index out of bounds")
@@ -26,3 +25,18 @@ class Array:
         temp_array[:self.max_size] = self.array
         self.array = temp_array
         self.max_size *= 2
+
+    def display(self):
+        print(" <-> ".join(str(self.array[i]) for i in range(self.index + 1)))
+
+my_array = Array(6)
+my_array.append(1)
+my_array.append(2)
+my_array.append(3)
+my_array.append(4)
+
+my_array.insert(88 ,2)
+
+my_array.display()
+
+
